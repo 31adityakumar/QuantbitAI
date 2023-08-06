97b727dea6a4fd94aee9f9dd10e0e603d584672d
Aditya <kumar.31aditya@gmail.com>
2023-08-07 00:24:29 +0530

    ADD: Hero Section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+import styles from './Hero.module.scss'
+const Hero = () => {
+    return(
+        <section className={styles.footer}>
+            Hero
+        </section>
+    )
+}
+
+export default Hero;
